57f0cbcbdb030b3e333c9e3d099941f1142b308a
daviddignam-translationie <40025154+daviddignam-translationie@users.noreply.github.com>
2018-06-07 14:14:15 +0100

    Add files via upload
    External Dependencies for the realex payment integration addon

---
+++ external dependencies/Installation — Wand 0.4.5_files/readthedocs-data.js
@@ -0,0 +1,14 @@
+  var READTHEDOCS_DATA = {
+    project: "wand",
+    version: "latest",
+    language: "en",
+    subprojects: {},
+    canonical_url: "http://docs.wand-py.org/en/0.4.4/",
+    theme: "flask",
+    builder: "sphinx",
+    docroot: "/docs/",
+    source_suffix: ".rst",
+    api_host: "https://readthedocs.org",
+    commit: "380068e9"
+  };
+  
